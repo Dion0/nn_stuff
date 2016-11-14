f86501bb2cf6993575e1b41185fd67fb923935c5
Dion0 <odv97ignis@gmail.com>
2016-11-14 19:16:30 +0300

    2 layers and stuff

--- convnet.py
+++ convnet.py
@@ -9,13 +9,14 @@ rng = np.random
 
 IMG_CLASSES = 10
 DELTA = 1.0
-training_steps = 2001
+training_steps = 3001
 reg = 5e-1
-step_size = 1e-0
+step_size = 2e-2
 
-H = 32
+H = 128
 
-W1 = 0.01 * np.random.randn(3073, IMG_CLASSES)
+W1 = 0.01 * np.random.randn(3073, H)
+W2 = 0.01 * np.random.randn(H, IMG_CLASSES)
 
 decode_list = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
@@ -31,21 +32,28 @@ def evaluate(inputs, outputs, w):
     np.equal(ans, outputs, ans)
     return np.sum(ans)
 
-def sample_images(ee, W1, cycles = 1000, bp_rate = 0.01, test = None):
+def sample_images(batch, ee, W1, W2, cycles = 1000, bp_rate = 0.01, test = None):
     pass
     if test == None:
         test = np.random.randn(10, 3073) * 0.01
     test[:][-1] = 1
     for i in range(cycles):
-        scores = np.dot(test, W1)
+        hidden_layer = np.maximum(0, np.dot(test, W1))
+        scores = np.dot(hidden_layer, W2)
+        scores -= scores.max()
         exp_scores = np.exp(scores)
         dscores = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
         dscores[range(10), range(10)] -= 1
         dscores /= 10
-        dX = np.dot(dscores, W1.T)
+
+        #dW2 = np.dot(hidden_layer.T, dscores)
+        dhidden = np.dot(dscores, W2.T)
+        dhidden[hidden_layer <= 0] = 0
+        dX = np.dot(dhidden, W1.T)
+
         test += -bp_rate * dX
     for j in range(test.shape[0]):
-        cifar.save_img(test[j], '{}_{}_out.png'.format(ee, j))
+        cifar.save_img(test[j], '{}_{}_{}_out.png'.format(batch, ee, decode_list[j]))
 
 
 dict2 = cifar.unpickle('test_batch')
@@ -56,7 +64,6 @@ del dict2
 ev_in -= np.mean(ev_in, axis = 0)
 ev_in /= (np.max(ev_in, axis=0) - np.min(ev_in, axis=0) + 1e-5) / 2
 ev_in = np.column_stack((ev_in, np.ones(ev_in.shape[0])))
-
 
 for batch_num in range(5):
     print('batch #', batch_num + 1)
@@ -73,37 +80,55 @@ for batch_num in range(5):
 
     for i in range(training_steps):
 
-        scores = np.dot(inputs, W1)
+        hidden_layer = np.maximum(0, np.dot(inputs, W1))
+        scores = np.dot(hidden_layer, W2)
+        scores -= scores.max()
         exp_scores = np.exp(scores)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
 
         correct_logprobs = -np.log(probs[range(num_inputs), outputs])
 
         data_loss = np.sum(correct_logprobs) / num_inputs
-        reg_loss = 0.5 * reg * (np.sum(W1[:, -1] * W1[:, -1]))
+        reg_loss = 0.5 * reg * (np.sum(W2[:, -1] * W2[:, -1]) + np.sum(W1[:, -1] * W1[:, -1]))
         loss = data_loss + reg_loss
         if i % 100 == 0:
-            with open('w1_', 'wb') as fw1:
+            with open('w1', 'wb') as fw1:
                 pickle._dump(W1, fw1)
+            with open('w2', 'wb') as fw2:
+                pickle._dump(W2, fw2)
 
             print("iteration %d: data_loss %f, loss %f" % (i, data_loss, loss))
             print(np.sum(scores.argmax(axis=1) == outputs))
-            scores = np.maximum(0, np.dot(ev_in, W1))
+            hidden_layer = np.maximum(0, np.dot(ev_in, W1))
+            scores = np.dot(hidden_layer, W2)
             print(np.sum(scores.argmax(axis=1) == ev_out))
-            sample_images(i, W1, 10050, 0.1)
+            sample_images(batch_num, i, W1, W2, 10050, 0.001)
+
 
         dscores = probs
         dscores[range(num_inputs), outputs] -= 1
         dscores /= num_inputs
 
-        dW1 = np.dot(inputs.T, dscores)
+        dW2 = np.dot(hidden_layer.T, dscores)
+        dhidden = np.dot(dscores, W2.T)
+        dhidden[hidden_layer <= 0] = 0
+        dW1 = np.dot(inputs.T, dhidden)
+
+        dW2[:, -1] += reg * W2[:, -1]  # don't forget the regularization gradient
         dW1[:, -1] += reg * W1[:, -1]
+
+        W2 += -step_size * dW2
         W1 += -step_size * dW1
 
-    scores = np.dot(inputs, W1)
+    hidden_layer = np.maximum(0, np.dot(inputs, W1))
+    scores = np.dot(hidden_layer, W2)
     print(np.sum(scores.argmax(axis=1) == outputs))
-    scores = np.dot(ev_in, W1)
+    hidden_layer = np.maximum(0, np.dot(ev_in, W1))
+    scores = np.dot(hidden_layer, W2)
     print(np.sum(scores.argmax(axis=1) == ev_out))
 
-with open('w1_', 'wb') as fw1:
+with open('w1', 'wb') as fw1:
     pickle._dump(W1, fw1)
+
+with open('w2', 'wb') as fw2:
+    pickle._dump(W2, fw2)
--- test.py
+++ test.py
@@ -5,121 +5,25 @@ import time
 import cifar
 import pickle
 import random
+import matplotlib.pyplot as plt
+import scipy.stats as st
+from scipy import ndimage
+from scipy.misc import imread, imsave, imresize, toimage
 
 
-rng = np.random
-#rng.seed(123)
+ar = np.array([
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 1, 2]
+])
+k = np.array([
+    [1, -1, -1],
+    [-1, 1, 0],
+    [0, 1, 1]
+])
+car = ndimage.correlate(ar, k, mode='constant', cval=0)
 
-IMG_CLASSES = 10
-REG_DELTA = 0.005
-DELTA = 1.0
-training_steps = 301
-learning_rate = 0.0001
-
-decode_list = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-
-#svm loss
-def loss(x, y, w):
-    scores = w.dot(x)
-    margins = np.maximum(0, scores - scores[y] + DELTA)
-    margins[y] = 0
-    return np.sum(margins) + np.sum(np.square(w)) * REG_DELTA / 2.0
-
-#all losses \ o /
-def total_loss(x, y, w):
-    loss_sum = 0
-    for inp, out in zip(x, y):
-        loss_sum += loss(inp, out, w)
-    return loss_sum
-
-def evaluate(inputs, outputs, w):
-    cnt = 0
-    for inp, out in zip(inputs, outputs):
-        tmp = w.dot(inp)
-        ans = np.argmax(tmp)
-        #print(ans, ' - ', out)
-        if out != ans:
-            cnt += 1
-    print(cnt)
-    return cnt
-
-#backprob and stuff
-def sample_image(ii, yi, w, cycles = 10000, bp_rate = 0.01, test = None):
-    if test == None:
-        test = np.random.randn(3073) * 0.01
-    test[:][-1] = 1
-    ans = [0] * IMG_CLASSES
-    ans[yi] = 1
-    for i in range(cycles):
-        scores = w.dot(x)
-        margins = scores - scores[yi] + DELTA > 0
-        margins[yi] = 0
-        gxy = -(np.sum(margins) - 1) * w[yi]
-        gx = (w.transpose() * margins).sum(axis=1) * bp_rate
-        np.add(gx, gxy, gx)
-        np.add(test, -gx, out=test)
-    tmp = w.dot(test)
-    test = test + 1.0
-    np.multiply(test, 128, test)
-    #test = test.astype(dtype=np.uint8)
-    cifar.save_img(test, 'wow{}_{}.png'.format(ii,decode_list[yi]))
-
-def wgrad(x, y, w):
-    scores = w.dot(x)
-    margins = scores - scores[y] + DELTA > 0
-    gwy = -(np.sum(margins) - 1) * x
-    grad = np.matmul(np.asmatrix(margins).transpose(), np.asmatrix(x))
-    grad[y] = gwy
-    #grad += w * REG_DELTA
-    grad[:][:-1] += w[:][:-1] * REG_DELTA
-    return grad
-
-def xgrad(x, y, w):
-    return w[y]
-    # scores = w.dot(x)
-    # margins = scores - scores[y] + DELTA > 0
-    # gwy = -(np.sum(margins) - 1) * x
-    # grad = np.matmul(np.asmatrix(margins).transpose(), np.asmatrix(x))
-    # grad[y] = gwy
-    # return grad
-
-def split_data(inp, out, batches_size = 256):
-    spl_l = []
-    ind = batches_size
-    while ind < inp.shape[0]:
-        spl_l.append(ind)
-        ind += batches_size
-    in_batch = np.split(inp, spl_l)
-    out_batch = np.split(out, spl_l)
-    return in_batch, out_batch
-
-
-
-dict2 = cifar.unpickle('test_batch')
-ev_in, ev_out = dict2['data'].astype(dtype=np.float64), dict2['labels']
-del dict2
-
-
-ev_in -= np.mean(ev_in, axis = 0)
-ev_in /= (np.max(ev_in, axis=0) - np.min(ev_in, axis=0)) / 2
-ev_in = np.column_stack((ev_in, np.ones(ev_in.shape[0])))
-
-# some constants
-SAMPLE_SIZE = len(ev_out)
-INPUT_NUM = len(ev_in[0])
-OUTPUT_NUM = IMG_CLASSES
-
-#initialize weights with random values (with normal distribution for some reason)
-w = rng.randn(OUTPUT_NUM, INPUT_NUM) * np.sqrt(2 / INPUT_NUM)
-
-with open('fullw_nonorm_3', 'rb') as fw:
-    w = pickle.load(fw)
-
-print(w)
-
-dict1 = cifar.unpickle("data_batch_1")
-outputs, r_inputs = dict1['labels'], dict1['data'].astype(dtype=np.float64)
-del dict1
-
-
+print(ar * k)
+print(k * ar)
+print(np.dot(ar, k))
+print(np.dot(k, ar))
